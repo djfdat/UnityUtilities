2c799578f8ad914f36a31351e1db67bfb1e3e3c7
Fatir Ahmad <djfdat@gmail.com>
2015-10-02 20:26:38 -0700

    Updated BitmaskExtensions.cs
    Fixed compile error
    Made this an extension method instead of a regular static method.

--- Assets/Scripts/Extensions/BitmaskExtensions.cs
+++ Assets/Scripts/Extensions/BitmaskExtensions.cs
@@ -3,27 +3,25 @@ using System.Collections;
 
 public static class BitmaskExtensions
 {
-  // Pass in a bitmask and return a specific bit
-  // Useful for getting a single enum value from a flag type enum field
-	public static int ReturnRandomBit(int bitmask)
+	// Pass in a bitmask and return a specific bit
+	// Useful for getting a single enum value from a flag type enum field
+	public static int ReturnRandomBit(this int bitmask)
 	{
-    int? selectedValue = null;
-    
-    for (int i = 0; ; i++)
-    {
-    	int checkVal = bitmask & 1;
-    	if (checkVal == 1)
-    	{
-    	  if (selectedValue = null || Random.Range(0, 2) == 0)
-    	  {
-    	    selectedValue = 1 << i;
-    	  }
-    	}
-    	bitmask = bitmask >> 1;
-    
-    	if (bitmask == 0) break;
-    }
-    
-    return selectedValue;
+		int? selectedValue = null;
+
+		for (int i = 0; bitmask != 0; i++)
+		{
+			int checkVal = bitmask & 1;
+			if (checkVal == 1)
+			{
+				if (selectedValue == null || Random.Range(0, 2) == 0)
+				{
+					selectedValue = 1 << i;
+				}
+			}
+			bitmask = bitmask >> 1;
+		}
+
+		return selectedValue != null ? (int)selectedValue : 0;
 	}
 }
